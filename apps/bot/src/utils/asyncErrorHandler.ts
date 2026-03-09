8511e1586a73d047b4dd2fe364306329fa1e090c
mridul-giri <mridulgiri2@gmail.com>
2026-03-09 11:33:54 +0530

    fix bug

--- apps/bot/src/utils/asyncErrorHandler.ts
+++ apps/bot/src/utils/asyncErrorHandler.ts
@@ -11,21 +11,24 @@ export const asyncErrorHandler = (handler: (ctx: Context) => Promise<void>) => {
       if (error instanceof BotError) {
         try {
           if (error.userMessage) await ctx.reply(error.userMessage);
-        } catch {}
-        console.error(`[BotError] ${error.logMessage || error.userMessage}`);
+        } catch {
+          console.error(`[BotError] ${error.logMessage || error.userMessage}`);
+        }
       } else {
         if (axios.isAxiosError(error)) {
           try {
             await ctx.reply(
               error.response?.data.message || BotReplies.UNEXPECTED_ERROR,
             );
-          } catch {}
-          console.error("[AxiosError]", error.response?.data.message);
+          } catch {
+            console.error("[AxiosError]", error.response?.data.message);
+          }
         } else {
           try {
             await ctx.reply(BotReplies.UNEXPECTED_ERROR);
-          } catch {}
-          console.error("[UnexpectedError]", error);
+          } catch {
+            console.error("[UnexpectedError]", error);
+          }
         }
       }
     }
